4dec1cde7691d5274d8646e139d8ad1be51c6884
wwd2022 <107018641+wwd2022@users.noreply.github.com>
2024-01-16 09:21:22 +0900

    [level Lv.1] Title: 정수 내림차순으로 배치하기, Time: 2.13 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cs
+++ 프로그래머스/Lv.1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cs
@@ -3,8 +3,8 @@ using System;
 public class Solution {
     public long solution(long n) {
         char[] answer = n.ToString().ToCharArray();
-        Array.Sort(answer);
-        Array.Reverse(answer);
+        System.Array.Sort(answer);
+        System.Array.Reverse(answer);
         return long.Parse(answer);
     }
 }
