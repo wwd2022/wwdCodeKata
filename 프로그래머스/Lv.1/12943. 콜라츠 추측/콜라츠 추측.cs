c2c8443dc9cc6b78a7f8b05674e3fe6d5127c053
wwd2022 <107018641+wwd2022@users.noreply.github.com>
2024-01-16 09:56:21 +0900

    [level Lv.1] Title: 콜라츠 추측, Time: 0.27 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12943. 콜라츠 추측/콜라츠 추측.cs
+++ 프로그래머스/Lv.1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int solution(long num) {
         if (num == 1) return 0;
-        for (int i = 1; i <= 500; i++)
+        for (int i = 1; i < 500; i++)
         {
             num = (num & 1) == 1 ? num * 3 + 1 : num / 2;
             if (num == 1) return i;
